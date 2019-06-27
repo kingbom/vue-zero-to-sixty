e142c7118ef2070e2d191cb3714301c9dc99757c
jaruwit suriyo <jaruwit.sur@ascendcorp.com>
2019-06-27 23:59:17 +0700

    create post service for call APIs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,22 @@
 <template>
     <h3>Home</h3>
 </template>
+
+<script>
+import PostService from '../PostService';
+const postService = new PostService();
+export default {
+    name : "Home",
+    data() {
+        return {
+          posts: []
+        }
+    },
+    created() {
+        postService
+            .getPosts()
+            .then(res => this.posts = res.data)
+            .catch(err => console.error(err));
+    }
+}
+</script>
